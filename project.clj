60540d70871610f8105ee2044d3749472b70d832
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-03-15 05:35:01 -0700

    Update to Clojure 1.5.1 and tools.reader 0.7.3

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :url "https://github.com/dakrone/clojuredocs-client"
   :description "A client for the clojuredocs API"
   ;; Note: The code worked fine with clojure version 1.3.0 as of Feb
-  ;; 23 2013.  The dependency on 1.5.0 is simply there to perform
+  ;; 23 2013.  The dependency on 1.5.1 is simply there to perform
   ;; a small amount of testing on a more recent Clojure version.
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [org.clojure/tools.reader "0.7.2"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.reader "0.7.3"]
                  [clj-http "0.3.5"]
                  [cheshire "3.1.0"]])
