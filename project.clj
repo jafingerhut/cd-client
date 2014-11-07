2616e8c61b1029ff8de17adaf35cc2c9cf7e6de1
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-11-07 01:14:53 -0800

    Bump up versions of nearly all libs

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
 (defproject org.thnetos/cd-client "0.3.5-SNAPSHOT"
   :url "https://github.com/dakrone/clojuredocs-client"
   :description "A client for the clojuredocs API"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.reader "0.7.6"]
-                 [clj-http "0.7.6"]
-                 [cheshire "5.2.0"]]
-  :profiles
-  {:dev
-   {:dependencies [[org.clojure/tools.trace "0.7.6"]
-                   ;; [clj-ns-browser "1.3.2-SNAPSHOT"]
-                   ]}})
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.reader "0.8.12"]
+                 [clj-http "1.0.1"]
+                 [cheshire "5.3.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.6"]
+                                  ;; [clj-ns-browser "1.3.2-SNAPSHOT"]
+                                  ]}})
